818052249f93868a92ce8a79d4c9d7480306e359
Steve Marshall <sjmarshalluk@Steves-MacBook-Air.local>
2014-10-02 15:59:43 +0100

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+rooms = Room.create(
+	[
+		{
+			title: "Tim",
+			address: "Andrews Road, London, E8 4RL",
+			description: "Bloody lovely place",
+			no_of_rooms: 5,
+			price_in_pence: 15000,
+			is_featured: false,
+			user_id: 1
+		},
+		{
+			title: "Steve",
+			address: "Vartry Road, London, N15 6HB",
+			description: "A bit shoddy",
+			no_of_rooms: 2,
+			price_in_pence: 75,
+			is_featured: true,
+			user_id: 1
+		}
+
+
+	]
+)
+
