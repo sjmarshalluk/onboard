5f3cae4907636a24e29806dd9252151112c7fb59
Steve Marshall <sjmarshalluk@Steves-MacBook-Air.local>
2014-10-02 17:23:32 +0100

    create in controller and new view for rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,15 +1,31 @@
 class RoomsController < ApplicationController
   def index
-  	@rooms = Room.where(title: 'Steve')
+  	@rooms = Room.all
   end
 
   def show
   end
-
+  
   def new
   	@room = Room.new
   end
 
+  def create
+  	@room = Room.new(room_params)
+  	if @room.save
+  		flash[:success] = "All done"
+  		redirect_to root_path
+  	else
+  		flash[:error] = "Nope"
+  		render :new
+  	end
+  end
+
   def edit
+  end
+
+  private
+  def room_params
+  	params.require(:room).permit(:title, :address, :description, :no_of_rooms, :price_in_pence, :user_id)
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,10 @@
 class Room < ActiveRecord::Base
+
+	validates :title, presence: true
+	validates :address, presence: true
+	validates :price_in_pence, presence: true
+	validates :no_of_rooms, presence: true
+	validates :is_featured, presence: true
+	validates :description, length: {minimum: 10}
+
 end
