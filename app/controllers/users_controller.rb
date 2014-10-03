c15b23d1eaa355e89ee5f50eca29c015428104c2
Steve Marshall <sjmarshalluk@Steves-MacBook-Air.local>
2014-10-03 12:13:10 +0100

    Add header styles and create users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
+
+
+  def create
+  	@user = User.new(user_params)
+  		if @user.save
+  			flash[:success] = "That worked"
+  			redirect_to root_path
+  		else
+  			flash[:error] = "Didnt work"
+  			render :new
+  		end
+  	end
+
+
+  	private
+  	def user_params
+  		params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+  	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
 	#http://api.rubyonrails.org/classes/ActiveModel/SecurePassword/ClassMethods.html
 	has_secure_password
 
+	validates :email, presence: true, uniqueness: true
+	validates :username, presence: true, uniqueness: true
+
 	has_many :rooms
 
 end
