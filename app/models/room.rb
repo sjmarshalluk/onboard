da74f3b956cb7eb07e20c3956fbefe85e29863be
Steve Marshall <sjmarshalluk@Steves-MacBook-Air.local>
2014-10-03 10:12:15 +0100

    adding validation

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,7 +4,10 @@ class Room < ActiveRecord::Base
 	validates :address, presence: true
 	validates :price_in_pence, presence: true
 	validates :no_of_rooms, presence: true
-	validates :is_featured, presence: true
+	# sayin that when it is created, the default is false (this is only on create)
+	validates :is_featured, presence: true #, inclusion: { in: [false]}, on: :create
+	# this is on update as well
+	#validates :is_featured, presence: true
 	validates :description, length: {minimum: 10}
 
 end
