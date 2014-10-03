2498ba8c0c0ab4c467b47556ad368322ea6731b9
Steve Marshall <sjmarshalluk@Steves-MacBook-Air.local>
2014-10-03 15:16:12 +0100

    helper methods for authorisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,29 @@ class ApplicationController < ActionController::Base
   	session[:user_id].present?
   end
 
+  def require_user
+  	unless logged_in?
+  		flash[:error] = "You need to be logged in"
+  		redirect_to new_session_path
+  	end
+  end
+
+  # to prevent non-owners of rooms from editing (via url)
+  def current_user_owns_room
+
+  	# Then find the room that is trying to be edited
+  	room = Room.find(params[:id])
+  	# And only allow if current user made that room
+  	unless logged_in? and current_user == room.user
+  		# Otherwise flash error and redirect to show page
+  		flash[:error] = "Only the owner can edit"
+  		# (room) indicates the requirement for an id form the url
+  		redirect_to room_path(room)  		
+  	end
+  end
+
   #these are now available everywhere as they are being defined in the Apllication controller
+  # helper_method :require_user
   helper_method :current_user
   helper_method :logged_in?
 
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,16 +1,27 @@
 class RoomsController < ApplicationController
+
+	# call the require_method helper method before new and create (rather than individually further down)
+	before_action :require_user, only: [:new, :create]
+	before_action :current_user_owns_room, only: [:edit, :update]
+	before_action :find_room, only: [:show, :edit, :update]
+
   def index
   	@rooms = Room.all
   end
 
   def show
+  	# Was first: @room = Room.find(params[:id])
+  	# Then to (with helper in appliaction controller): find_room
+  	# Then moved to top
   end
   
   def new
+  	# require_user
   	@room = Room.new
   end
 
   def create
+  	# require_user
   	@room = Room.new(room_params)
   	if @room.save
   		flash[:success] = "All done"
@@ -22,8 +33,30 @@ class RoomsController < ApplicationController
   end
 
   def edit
+  	# moved to top: current_user_owns_room
+  	# moved to top: @room = Room.find(params[:id])
   end
 
+  def update
+  	# moved to top: current_user_owns_room
+  	# same as new apart from (same for most apps)
+  	# .new beceomes .find
+  	# .save becomes .update
+  	# room_params moves to update
+  	# moved to top: @room = Room.find(params[:id])
+  	if @room.update(room_params)
+  		flash[:success] = "All done"
+  		redirect_to root_path
+  	else
+  		flash[:error] = "Nope"
+  		render :new
+  	end
+  end
+
+  def find_room
+  	@room = Room.find(params[:id])
+  end
+  
   private
   def room_params
   	params.require(:room).permit(:title, :address, :description, :no_of_rooms, :price_in_pence, :user_id)
